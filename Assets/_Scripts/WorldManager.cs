716becc7caeba53911c74c4f47003450d0c4dbd6
Paul Niezborala <paul.niezborala@yahoo.fr>
2016-01-21 17:26:18 +0100

    Simple characters and movement

---
+++ Assets/_Scripts/WorldManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldManager : MonoBehaviour {
+    public Transform player;
+	// Use this for initialization
+	void Start () {
+        Instantiate(player, new Vector3(10, 0, 10), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CreatePlayer()
+    {
+        Instantiate(player, new Vector3(0,0,0), Quaternion.identity);
+
+    }
+}
